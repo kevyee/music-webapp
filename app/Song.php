694117747bd56e247b184ac0531cc107220f2843
Kevin Yee <kkevinyee13@gmail.com>
2016-10-30 17:47:51 +1100

    remote

--- app/Song.php
+++ app/Song.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Song extends Model
 {
-    //
+    public $timestamps = false;
+    protected $table = 'song';
+    protected $primaryKey = 'song_id';
 }
