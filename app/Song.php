3522bbcbd5caf0eef835ebad4128e515ed42aab2
Kevin Yee <kkevinyee13@gmail.com>
2016-11-06 17:42:34 +1100

    changed secure_asset

--- app/Song.php
+++ app/Song.php
@@ -9,4 +9,23 @@ class Song extends Model
     public $timestamps = false;
     protected $table = 'song';
     protected $primaryKey = 'song_id';
+
+    protected $fillable = [
+        'song_id', 
+        'gnre_id', 
+        'song_file_name', 
+        'song_file_image', 
+        'total_stream_count', 
+        'user_id', 
+        'song_status_id',  
+        'created', 
+        'updated',
+    ];
+
+    public $rules = [
+        'gnre_id' => 'required', 
+        'song_file_name' => 'required', 
+        'song_file_image' => 'required',
+        'gnre_id' => 'required',
+    ];
 }
