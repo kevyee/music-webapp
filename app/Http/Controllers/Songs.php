fb19afb3bd8d27327c00cf784d40e2a54b56e92d
Kevin Yee <kkevinyee13@gmail.com>
2016-11-12 19:16:25 +1100

    hello

--- app/Http/Controllers/Songs.php
+++ app/Http/Controllers/Songs.php
@@ -39,12 +39,13 @@ class Songs extends Controller
     }
 
     public function store(Request $request) {
+        return response(Input::all(), 200);
         if(!Input::hasFile('file')){
             $this->validator->addError('Please include the song file.');
             return response($this->validator->errors(), 403);
         }
-        return response($request, 200);
         
+
         $song_file = Input::file('file');
         $file_limit = 5000;
         $songFileType = $song_file->getClientOriginalExtension();
