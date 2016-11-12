4769ae2c93c29e2792bea3e20d6d5950566d1909
Kevin Yee <kkevinyee13@gmail.com>
2016-11-12 15:17:19 +1100

    removed folder

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -11,10 +11,8 @@
     <link rel="stylesheet" href="{{ asset('css/css_r/font.css') }}" type="text/css" />
     <link rel="stylesheet" href="{{ asset('css/css_r/app.css') }}" type="text/css" />
 
-    <script>var baseUrl = "{{ url('/') }}";</script> // $url = ;
-
+    <script>var baseUrl = "{{ url('/') }}";</script> 
     <script>var pubUrl = "{{ public_path() }}";</script> 
-    <script>var asdf = "{{ var_dump(parse_url(getenv('CLEARDB_DATABASE_URL'))) }}";</script> 
     <script>var assetUrl = "{{ asset('') }}";</script> 
     
 
